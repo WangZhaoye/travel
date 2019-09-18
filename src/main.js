98e004d6f0748eb44392786e7ed81514db3cd66e
develop <wangzhaoye@smail.nju.edu.cn>
2019-09-18 23:37:59 +0800

    init my project

---
+++ src/main.js
@@ -0,0 +1,19 @@
+
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+import fastClick from 'fastclick'
+import 'styles/reset.css'
+import 'styles/border.css'
+import 'styles/iconfont.css'
+
+Vue.config.productionTip = false
+fastClick.attach(document.body)
+
+
+new Vue({
+  el: '#app',
+  router,
+  components: { App },
+  template: '<App/>'
+})
